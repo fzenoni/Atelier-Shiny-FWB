4e3b60208cba59c29e68432272f006cc8a771cfa
Florian Zenoni <florian.zenoni@cfwb.be>
2026-04-07 11:10:25 +0200

    style: apply air

--- solutions/partie1_exercice1_solution_app.R
+++ solutions/partie1_exercice1_solution_app.R
@@ -2,21 +2,18 @@ library(shiny)
 
 # Define UI ----
 ui <- page_fluid(
-
-  textInput("nom",
-           "Votre nom"),
-  selectInput("cat",
-              "Catégorie",
-              choices = c("Générale", "Développement", "Déploiement")),
+  textInput("nom", "Votre nom"),
+  selectInput(
+    "cat",
+    "Catégorie",
+    choices = c("Générale", "Développement", "Déploiement")
+  ),
   textAreaInput("question", "Question"),
   actionButton("bouton", "Envoyer")
-
-
 )
 
 # Define server (empty) ----
-server <- function(input, output, session) {
-}
+server <- function(input, output, session) {}
 
 # Create Shiny app ----
 shinyApp(ui, server)
